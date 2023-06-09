69955206e282a5e47763ef3fa183693387ed4e92
Matheus <matheusalmeida016@gmail.com>
2023-06-09 19:05:07 -0400

    chore(header): add postcss, css variables and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })],
+  experimental: {
+    assets: true,
+  },
+})
--- src/components/home/home.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-export const Home = () => {
-  const [count, setCount] = useState(0)
-
-  return (
-    <button onClick={() => setCount(prev => prev + 1)}>{count}</button>
-  )
-}
