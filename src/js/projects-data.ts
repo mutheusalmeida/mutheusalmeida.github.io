26dca2205a95394da5ccd153ee1fe89aea9459d4
Matheus <matheusalmeida016@gmail.com>
2023-06-12 12:32:49 -0400

    feat: add projects section

---
+++ src/js/projects-data.ts
@@ -0,0 +1,40 @@
+import pawxySearch from '@/assets/pawxy-search.png'
+import dekoder from '@/assets/dekoder.png'
+import quizzical from '@/assets/quizzical.png'
+import markee from '@/assets/markee.png'
+import notee from '@/assets/notee.png'
+
+const data = [
+  {
+    path: 'https://github.com/mutheusalmeida/pawxy-search',
+    image: pawxySearch,
+    alt: 'Pawxy search screenshot',
+    title: 'Pawxy search',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/dekoder/',
+    image: dekoder,
+    alt: 'dekoder screenshot',
+    title: 'dekoder',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/quizzical-app',
+    image: quizzical,
+    alt: 'Quizzical screenshot',
+    title: 'Quizzical',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/markee',
+    image: markee,
+    alt: 'Markee screenshot',
+    title: 'Markee',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/notee',
+    image: notee,
+    alt: 'Notee screenshot',
+    title: 'Notee',
+  },
+]
+
+export default data
