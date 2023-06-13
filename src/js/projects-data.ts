041608a1e03a9c9ae178b3289243d04fa88af211
Matheus <matheusalmeida016@gmail.com>
2023-06-13 10:31:36 -0400

    improve(projects): add projects date and sort it

--- src/js/projects-data.ts
+++ src/js/projects-data.ts
@@ -1,6 +1,5 @@
 import pawxySearch from '@/assets/pawxy-search.png'
 import dekoder from '@/assets/dekoder.png'
-import quizzical from '@/assets/quizzical.png'
 import markee from '@/assets/markee.png'
 import notee from '@/assets/notee.png'
 
@@ -10,30 +9,28 @@ const data = [
     image: pawxySearch,
     alt: 'Pawxy search screenshot',
     title: 'Pawxy search',
-  },
-  {
-    path: 'https://github.com/mutheusalmeida/dekoder/',
-    image: dekoder,
-    alt: 'dekoder screenshot',
-    title: 'dekoder',
-  },
-  {
-    path: 'https://github.com/mutheusalmeida/quizzical-app',
-    image: quizzical,
-    alt: 'Quizzical screenshot',
-    title: 'Quizzical',
-  },
-  {
-    path: 'https://github.com/mutheusalmeida/markee',
-    image: markee,
-    alt: 'Markee screenshot',
-    title: 'Markee',
+    date: 'May 23, 2023, 2:04 PM GMT-4',
   },
   {
     path: 'https://github.com/mutheusalmeida/notee',
     image: notee,
     alt: 'Notee screenshot',
     title: 'Notee',
+    date: 'Aug 6, 2021, 12:45 AM GMT-4',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/dekoder/',
+    image: dekoder,
+    alt: 'dekoder screenshot',
+    title: 'dekoder',
+    date: 'May 11, 2023, 3:07 PM GMT-4',
+  },
+  {
+    path: 'https://github.com/mutheusalmeida/markee',
+    image: markee,
+    alt: 'Markee screenshot',
+    title: 'Markee',
+    date: 'Sep 14, 2021, 3:40 AM GMT-4',
   },
 ]
 
