2c5b73048a053c54acb73ed6116a46197e0d9ec7
Matheus <matheusalmeida016@gmail.com>
2023-06-26 15:04:46 -0400

    feat: add prettierrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import image from "@astrojs/image"
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,52 +1,52 @@
-import { ui, defaultLang } from "@/i18n/ui";
+import { ui, defaultLang } from '@/i18n/ui'
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split("/");
+  const [, lang] = url.pathname.split('/')
 
-  if (lang in ui) return lang as keyof typeof ui;
+  if (lang in ui) return lang as keyof typeof ui
 
-  return defaultLang;
+  return defaultLang
 }
 
 export function removeLeadingSlash(path: string) {
-  return path.replace(/^[/\\]+/, "");
+  return path.replace(/^[/\\]+/, '')
 }
 
 export function removeTrailingSlash(path: string) {
-  return path.replace(/[/\\]+$/, "");
+  return path.replace(/[/\\]+$/, '')
 }
 
 export function l(url: URL, lang: keyof typeof ui) {
-  const currentLang = getLangFromUrl(url);
-  let pathname = removeTrailingSlash(url.pathname);
+  const currentLang = getLangFromUrl(url)
+  let pathname = removeTrailingSlash(url.pathname)
 
   if (currentLang !== lang) {
-    pathname = removeLeadingSlash(pathname);
+    pathname = removeLeadingSlash(pathname)
 
     if (lang === defaultLang) {
-      pathname = `/${pathname.split("/").slice(1).join("/")}`;
+      pathname = `/${pathname.split('/').slice(1).join('/')}`
     } else {
-      pathname = pathname ? `/${lang}/${pathname}` : `/${lang}`;
+      pathname = pathname ? `/${lang}/${pathname}` : `/${lang}`
     }
 
-    return pathname;
+    return pathname
   }
 
-  return pathname;
+  return pathname
 }
 
 export function normalizeLangTag(tag: string) {
-  if (!tag.includes("-")) return tag.toLowerCase();
+  if (!tag.includes('-')) return tag.toLowerCase()
 
-  const [lang, region] = tag.split("-");
+  const [lang, region] = tag.split('-')
 
-  return lang.toLowerCase() + "-" + region.toUpperCase();
+  return lang.toLowerCase() + '-' + region.toUpperCase()
 }
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  };
+    return ui[lang][key] || ui[defaultLang][key]
+  }
 }
 
 export function formatDate(
@@ -55,11 +55,11 @@ export function formatDate(
   options?: Intl.DateTimeFormatOptions
 ) {
   const config = options || {
-    month: "short",
-    year: "numeric",
-  };
+    month: 'short',
+    year: 'numeric',
+  }
 
-  const formatedDate = data.toLocaleString(lang, config);
+  const formatedDate = data.toLocaleString(lang, config)
 
-  return formatedDate;
+  return formatedDate
 }
